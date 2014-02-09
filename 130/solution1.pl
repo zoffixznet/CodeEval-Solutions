93d754511956750a5edeef10f296bcfcad03953f
Zoffix Znet <cpan@zoffix.com>
2014-02-09 12:49:53 -0500

    Non-working crap

--- 130/solution1.pl
+++ 130/solution1.pl
@@ -7,11 +7,36 @@ my $filename = shift;
 open my $fh, '<', $filename
     or die "Failed to open $filename: $!\n";
 
-use re::engine::RE2;
 while (<$fh>) {
     chomp;
     my ( $pattern, $text ) = split ' ';
-    $pattern =~ s/0/A+/g;
-    $pattern =~ s/1/(A+|B+)/g;
-    print $text =~ /^$pattern$/ ? 'Yes' : 'No', "\n";
+    my $status = 'Yes';
+    while ( length $pattern ) {
+        if ( $pattern =~ /^0(?=0)/ and $text =~ /^A/ ) {
+            $pattern =~ s/^0(?=0)//;
+            $text    =~ s/^A//;
+        }
+        elsif ( $pattern =~ /^0(?!0)/ and $text =~ /^A+/ ) {
+            $pattern =~ s/^0(?!0)//;
+            $text    =~ s/^A+//;
+        }
+        elsif ( $pattern =~ /^1(?=1(0|\z))/ and $text =~ /^([AB])\1*([AB])\2*(?=A)/ ) {
+            $pattern =~ s/^1(?=1(0|\z))//;
+            $text    =~ s/^([AB])\1*([AB])\2*(?=A)//;
+        }
+        elsif ( $pattern =~ /^1(?=1(1|\z))/ and $text =~ /^[AB]/ ) {
+            $pattern =~ s/^1(?=1(1|\z))//;
+            $text    =~ s/^[AB]//;
+        }
+        elsif ( $pattern =~ /^1\z/ and $text =~ /^[AB]/ ) {
+            $pattern =~ s/^1\z//;
+            $text =~ s/^[AB]//;
+        }
+        else {
+            $status = 'No';
+            last;
+        }
+    }
+    $status = 'No' if length $text;
+    print "$status\n";
 }
